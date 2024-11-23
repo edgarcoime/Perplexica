338d414091ec04f6aa7695daa855050fb61760d1
Edgar Coime <e.coimeii@hotmail.com>
2024-11-22 21:24:21 -0800

    fix: enabled local building of server

--- backend.dockerfile
+++ backend.dockerfile
@@ -13,4 +13,13 @@ RUN mkdir /home/perplexica/data
 RUN yarn install --frozen-lockfile --network-timeout 600000
 RUN yarn build
 
+# Copy and prepare the initialization script
+COPY init_config.sh /home/perplexica/
+COPY sample.config.toml /home/perplexica/
+RUN chmod +x /home/perplexica/init_config.sh
+
+# If you want env vars from docker-compose needs to be 
+# in entry point. Docker caches previous calls so not possible there
+# https://stackoverflow.com/questions/55921914/how-to-source-a-script-with-environment-variables-in-a-docker-build-process
+ENTRYPOINT ["/home/perplexica/init_config.sh"]
 CMD ["yarn", "start"]
---
+++ init_config.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Define the log file path
+LOG_FILE="/var/log/init.log"
+
+# Redirect all output to the log file
+exec > "$LOG_FILE" 2>&1
+
+# Define the path to the configuration file
+SAMPLE_CONFIG="sample.config.toml"
+CONFIG_FILE="config.toml"
+
+# Check if the sample config file exists
+if [ ! -f "$SAMPLE_CONFIG" ]; then
+    echo "Error: $SAMPLE_CONFIG not found."
+    exit 1
+fi
+
+# # Copy sample.config.toml to config.toml
+cp "$SAMPLE_CONFIG" "$CONFIG_FILE"
+echo "Copied $SAMPLE_CONFIG to $CONFIG_FILE."
+
+echo "openai: ${OPENAI:-}"
+echo "groq: ${GROQ:-}"
+echo "anth: ${ANTHROPIC:-}"
+
+# Detect the operating system
+OS=$(uname)
+
+# Use different sed options based on OS
+if [ "$OS" == "Darwin" ]; then
+    # macOS sed command
+    sed -i '' "s|OPENAI = .*|OPENAI = \"${OPENAI:-}\"|g" "$CONFIG_FILE"
+    sed -i '' "s|GROQ = .*|GROQ = \"${GROQ:-}\"|g" "$CONFIG_FILE"
+    sed -i '' "s|ANTHROPIC = .*|ANTHROPIC = \"${ANTHROPIC:-}\"|g" "$CONFIG_FILE"
+else
+    # Linux sed command
+    sed -i "s|OPENAI = .*|OPENAI = \"${OPENAI:-}\"|g" "$CONFIG_FILE"
+    sed -i "s|GROQ = .*|GROQ = \"${GROQ:-}\"|g" "$CONFIG_FILE"
+    sed -i "s|ANTHROPIC = .*|ANTHROPIC = \"${ANTHROPIC:-}\"|g" "$CONFIG_FILE"
+fi
+
+echo "Populated $CONFIG_FILE with API keys (or defaults)."
+
+# Then run the CMD
+exec "$@"
